839058ac39a79680c6914a6821999b99f10c76ed
jheaton <jheaton@CORP.AMAZONWORKSPACES.COM>
2018-04-24 05:56:10 -0500

    Java version can now evolve rules.

--- java/evolve/src/main/java/org/heatonresearch/mergelife/MergeLife.java
+++ java/evolve/src/main/java/org/heatonresearch/mergelife/MergeLife.java
@@ -3,12 +3,16 @@ package org.heatonresearch.mergelife;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class MergeLife {
 	private final List<MergeLifeGenome> population = new ArrayList<>();
 	private final ObjectiveFunction objectiveFunction;
+	private final int CUT_LENGTH = 5;
+	private int evalCount;
+	private MergeLifeGenome topGenome;
 
 	public MergeLife(Random rnd, int populationSize, String objectiveFilename) throws IOException {
 		this.objectiveFunction = new ObjectiveFunction(objectiveFilename);
@@ -17,8 +21,10 @@ public class MergeLife {
 		}
 	}
 
-	public MergeLifeGenome tournament(Random rnd, boolean chooseBest, int cycles) {
+	public MergeLifeGenome tournament(Random rnd, int cycles) {
 		MergeLifeGenome best = null;
+		this.evalCount++;
+		System.out.println("Eval " + this.evalCount + ":" + this.topGenome);
 
 		for(int i = 0;i<cycles;i++) {
 			int idx = (int)(rnd.nextDouble()*(this.population.size()));
@@ -26,16 +32,11 @@ public class MergeLife {
 			challenger.calculateScore(this.objectiveFunction);
 
 			if( best!=null ) {
-				if (chooseBest) {
-					//System.out.println("Best: " + best);
-					//System.out.println("Challenger:" + challenger);
-					if( best.compareTo(challenger)<0) {
-						best = challenger;
-						//System.out.println("Won!");
-					}
-				} else {
-					if( best.compareTo(challenger)>0) {
-						best = challenger;
+				if( best.compareTo(challenger)<0) {
+					best = challenger;
+					if(this.topGenome == null || best.getScore()>this.topGenome.getScore()) {
+						this.topGenome = best;
+						System.out.println("New best: " + this.topGenome);
 					}
 				}
 			} else {
@@ -47,18 +48,78 @@ public class MergeLife {
 	}
 
 	public String mutate(Random rnd, String parent) {
-		return "";
+		final String h = "0123456789ABCDEF";
+		String result = "";
+
+		boolean done = false;
+		while( !done ) {
+			int i = (int) (rnd.nextDouble() * parent.length());
+
+			if (parent.charAt(i) != '-') {
+				int i2 = (int) (rnd.nextDouble() * h.length());
+				result = parent.substring(0, i) + h.charAt(i2) + parent.substring(i + 1);
+				if (!result.equalsIgnoreCase(parent)) {
+					done = true;
+				}
+			}
+		}
+		return result;
+	}
+
+	public void addChild(Random rnd, String ruleText) {
+		MergeLifeGenome genome = new MergeLifeGenome(ruleText);
+		genome.calculateScore(this.objectiveFunction);
+		int worstIdx =-1;
+
+		synchronized (this.population) {
+			MergeLifeGenome worst = null;
+
+			for(int i = 0;i<5;i++) {
+				int idx = (int)(rnd.nextDouble()*(this.population.size()));
+				MergeLifeGenome challenger = this.population.get(idx);
+				challenger.calculateScore(this.objectiveFunction);
+
+				if( worst!=null ) {
+					if( worst.compareTo(challenger)>0) {
+						worst = challenger;
+						worstIdx = idx;
+					}
+				} else {
+					worst = challenger;
+					worstIdx = idx;
+				}
+			}
+
+			this.population.remove(worstIdx);
+			this.population.add(worstIdx,genome);
+		}
 	}
 
 	public String[] crossover(Random rnd, String parent1, String parent2) {
-		return new String[2];
+		// The genome must be cut at two positions, determine them
+		int cutpoint1 = (int)(rnd.nextDouble() * (parent1.length() - CUT_LENGTH));
+		int cutpoint2 = cutpoint1 + CUT_LENGTH;
+
+		// Produce two offspring
+		String[] r = new String[2];
+		r[0] = parent1.substring(0,cutpoint1) + parent2.substring(cutpoint1,cutpoint2) + parent1.substring(cutpoint2);
+		r[1] = parent2.substring(0,cutpoint1) + parent1.substring(cutpoint1,cutpoint2) + parent2.substring(cutpoint2);
+		return r;
 	}
 
 	public void step(Random rnd) {
 		if( rnd.nextDouble()<0.75) {
-			// crossover
+			// Crossover
+			MergeLifeGenome p1 = tournament(rnd,5);
+			MergeLifeGenome p2 = tournament(rnd,5);
+			String[] c = crossover(rnd,p1.getRuleText(),p2.getRuleText());
+			addChild(rnd,c[0]);
+			addChild(rnd,c[1]);
 		} else {
 			// mutation
+			MergeLifeGenome p1 = tournament(rnd,5);
+			String c = mutate(rnd, p1.getRuleText());
+			addChild(rnd,c);
 		}
 	}
 
@@ -80,8 +141,11 @@ public class MergeLife {
 
 	public static void main(String[] args) {
 		try {
-			MergeLife life = new MergeLife( new Random(),100, "D:\\Users\\jheaton\\projects\\mergelife\\java\\evolve\\paperObjective.json");
-			System.out.println(life.tournament(new Random(),true,5));
+			Random rnd = new Random();
+			MergeLife life = new MergeLife( rnd,100, "D:\\Users\\jheaton\\projects\\mergelife\\java\\evolve\\paperObjective.json");
+			for(;;) {
+				life.step(rnd);
+			}
 		} catch(Exception ex) {
 			ex.printStackTrace();
 		}
