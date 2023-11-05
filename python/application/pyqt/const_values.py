994e130a456426d3bdf0552bf17876a23cc195ef
Jeff Heaton <jeff@jeffheaton.com>
2023-11-05 13:48:25 -0600

    Updated about box

--- python/application/pyqt/const_values.py
+++ python/application/pyqt/const_values.py
@@ -3,6 +3,8 @@ import utl_env
 
 BUNDLE_ID = "com.heatonresearch.heaton-ca"
 APP_ID = BUNDLE_ID.split('.')[-1]
+APP_NAME = "HeatonCA"
+COPYRIGHT = "Copyright 2023 by Jeff Heaton, released under the <a href='https://opensource.org/license/mit/'>MIT License</a>"
 VERSION = "1.0.0"
 
 if utl_env.is_sandboxed():
--- python/application/pyqt/tab_about.py
+++ python/application/pyqt/tab_about.py
@@ -1,17 +1,32 @@
-import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QTabWidget, QVBoxLayout, QWidget, QLabel
+from PyQt6.QtCore import Qt
+import const_values
 
 class AboutTab(QWidget):
     def __init__(self):
         super().__init__()
 
+        text = f"""
+<H1>{const_values.APP_NAME} {const_values.VERSION}</H1>
+{const_values.COPYRIGHT}
+<br>
+<hr>
+This program implements the cellular automata described in the paper:<br>
+Heaton, J. March (2018). Evolving continuous cellular automata for aesthetic objectives. <i>Genetic Programming Evolvable Machines<i>.<br><a href='https://doi.org/10.1007/s10710-018-9336-1'>https://doi.org/10.1007/s10710-018-9336-1</a>
+<hr>
+Log path: {const_values.LOG_DIR}
+""" 
+
         # Create the QLabel with the hyperlink
-        self.label = QLabel("<a href='http://example.com'>Visit our website!</a>", self)
+        self.label = QLabel(text, self)
         self.label.setOpenExternalLinks(True)
         
         # Set a layout for the CustomTab and add the label to it
         layout = QVBoxLayout(self)
         layout.addWidget(self.label)
+
+        # Align the content to the top
+        layout.setAlignment(Qt.AlignmentFlag.AlignTop)
 
     def on_close(self):
         # Your custom functionality here
