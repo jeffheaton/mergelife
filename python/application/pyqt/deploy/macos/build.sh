3809dfd7b112b0e2540134a8261a6e73683b3565
Jeff Heaton <jeff@jeffheaton.com>
2026-07-21 08:21:03 -0500

    Build fix

--- python/application/pyqt/deploy/macos/build.sh
+++ python/application/pyqt/deploy/macos/build.sh
@@ -17,7 +17,6 @@ mkdir ./working
 cp ./entitlements.plist ./working
 cp ./heaton_ca_icon.icns ./working
 cp ./heaton-ca-macos.spec ./working
-cp ./build.py ./working
 cp ./build.sh ./working
 cp ../../*.py ./working
 cp -r ../../data ./working/data
